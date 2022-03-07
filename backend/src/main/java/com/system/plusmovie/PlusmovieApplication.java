e39bf646b854c1e7555230423669727ca2cd8f8e
Edmundo Lima <edmundolima021@gmail.com>
2022-03-07 15:21:54 -0300

    Project created

---
+++ backend/src/main/java/com/system/plusmovie/PlusmovieApplication.java
@@ -0,0 +1,13 @@
+package com.system.plusmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlusmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlusmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/system/plusmovie/PlusmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.plusmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlusmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

8c32ed918bc1236aa1e160708d43841d71d6816d
Edmundo Lima <edmundolima021@gmail.com>
2022-03-11 16:39:34 -0300

    useParams

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,46 +1,14 @@
-import { Link } from 'react-router-dom';
-import './styles.css';
+import FormCard from 'components/FormCard';
+import { useParams } from 'react-router-dom';
 
 function Form() {
 
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+    const params = useParams();
 
     return (
-        <div className="plusmovie-form-container">
-            <img className="plusmovie-movie-card-image" src={movie.image} alt={movie.title} />
-            <div className="plusmovie-card-bottom-container">
-                <h3>{movie.title}</h3>
-                <form className="plusmovie-form">
-                    <div className="form-group plusmovie-form-group">
-                        <label htmlFor="email">Informe seu email</label>
-                        <input type="email" className="form-control" id="email" />
-                    </div>
-                    <div className="form-group plusmovie-form-group">
-                        <label htmlFor="score">Informe sua avaliação</label>
-                        <select className="form-control" id="score">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </select>
-                    </div>
-                    <div className="plusmovie-form-btn-container">
-                        <button type="submit" className="btn btn-primary plusmovie-btn">Salvar</button>
-                    </div>
-                </form >
-                <Link to="/">
-                    <button className="btn btn-primary plusmovie-btn mt-3">Cancelar</button>
-                </Link>
-            </div >
-        </div >
+        <FormCard movieId={`${params.movieId}`} />
     );
 
 }
 export default Form;
+
